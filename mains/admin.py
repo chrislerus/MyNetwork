7527d12044a1d8c2889164108a3f285156048f70
Chris <lerus.chris@gmail.com>
2014-04-03 20:50:48 +0200

    +1 vote Answers gestions Profile page

--- mains/admin.py
+++ mains/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from mains.models import Question, Answer, Asker
+from mains.models import Question, Answer, Asker, Vote
 
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Asker)
+admin.site.register(Vote)
--- mains/views.py
+++ mains/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response, redirect
-from mains.models import Answer, Question, User, Asker
+from mains.models import Answer, Question, Asker, Vote
 from django.http import *
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
@@ -38,57 +38,68 @@ def homepage(request):
                               {'page_title': 'Welcome', 'current_user': request.user},
                               context_instance=RequestContext(request))
 
+
 # Display friends. Not handled if no user
-def friends(request):
+def profile(request, username):
     for asker in Asker.objects.all():
-        if request.user == asker.user:
+        if username == asker.user.username:
             current_user = asker
             break
     l_friends = current_user.friends.all()
-    return render_to_response('mains/friends.html',
-        {'l_friends': l_friends, 'page_title': "My Profile"},
+    return render_to_response('mains/profile.html',
+        {'l_friends': l_friends, 'page_title': "My Profile",  'current_user': request.user, 'profile': current_user},
         context_instance=RequestContext(request)
         )
+
 
 # Display all questions
 def all_questions_by_votes(request):
     l_quests = Question.objects.exclude(target=request.user.id).order_by('-votes')
     return render_to_response('mains/questions_votes.html',
-        {"l_quests": l_quests, "page_title": "Questions World"},
+        {"l_quests": l_quests, "page_title": "Questions World", 'current_user': request.user},
         context_instance=RequestContext(request))
+
 
 # Display question to vote ordered by time (Id)
 def all_questions_by_time(request):
     l_quests = Question.objects.exclude(target=request.user.id).order_by('-question_id')
     return render_to_response('mains/questions_votes.html',
-        {"l_quests": l_quests, "page_title": "Questions World"},
+        {"l_quests": l_quests, "page_title": "Questions World", 'current_user': request.user},
         context_instance=RequestContext(request))
+
 
 # Display questions asked by people, ordered by votes
 def tome_questions_by_votes(request):
     l_quests = Question.objects.filter(target=request.user.id).order_by('-votes')
     return render_to_response('mains/questions_answers.html',
-        {"l_quests": l_quests, "page_title": "Questions To Me"},
+        {"l_quests": l_quests, "page_title": "Questions To Me", 'current_user': request.user},
         context_instance=RequestContext(request))
+
 
 # Display questions asked by people, ordered by time (Id)
 def tome_questions_by_time(request):
 
     l_quests = Question.objects.filter(target=request.user.id).order_by('-question_id')
     return render_to_response('mains/questions_answers.html',
-        {"l_quests": l_quests, "page_title": "Questions To Me"},
+        {"l_quests": l_quests, "page_title": "Questions To Me", 'current_user': request.user},
         context_instance=RequestContext(request))
 
+
+# TODO : Disable button if already voted. How? check vote table
 def detail_answer(request, question_id):
-    quest = Question.objects.get(pk=question_id)
-    answers = Answer.objects.all()
-    if request.POST:
-        for asker in Asker.objects.all():
+    current_quest = Question.objects.get(pk=question_id)
+    answers = Answer.objects.filter(question=current_quest)
+    for asker in Asker.objects.all():
             if asker.user == request.user:
                 current_asker = asker
-        current_quest = Question.objects.get(pk=question_id)
-        Answer.objects.create(user_id=current_asker, question=current_quest, text=request.POST['answer'])
-        return HttpResponseRedirect('questions/tome/')
-    return render_to_response('mains/question_detail_answer.html',
-        {"quest": quest, "page_title": "Detail Question", "answers": answers},
+    if request.POST:
+        if 'btn_vote' in request.POST:
+            current_quest.votes += 1
+            current_quest.save()
+            Vote.objects.create(user_id=current_asker, question_id=current_quest)
+        elif 'btn_answer' in request.POST:
+            Answer.objects.create(user_id=current_asker, question=current_quest, text=request.POST['answer'])
+            return HttpResponseRedirect('questions/tome/')
+    return render_to_response('mains/question_details.html',
+        {"quest": current_quest, "page_title": "Detail Question", "answers": answers, 'current_user': request.user},
         context_instance=RequestContext(request))
